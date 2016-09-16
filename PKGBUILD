e598934d41aee3277f94a1e06ea5d2c05432d7dd
torben <t.sickert[~at~]gmail.com>
2016-09-16 20:46:14 +0200

    a

--- PKGBUILD
+++ PKGBUILD
@@ -10,23 +10,25 @@
 # 3.0 unported license. see http://creativecommons.org/licenses/by/3.0/deed.de
 # endregion
 pkgname=backup-rotation
-pkgver=1.0
-pkgrel=1
+pkgver=1.0.2
+pkgrel=5
 pkgdesc='This script allows you to create a local or remote backup rotation for your files.'
 arch=('any')
 url='http://torben.website/backupRotation'
 license=('CC-BY-3.0')
 depends=('pacman' 'yaourt' 'rsync' 'findutils')
-source=('https://raw.githubusercontent.com/thaibault/backupRotation/master/backupRotation.sh' 'https://raw.githubusercontent.com/thaibault/backupRotation/master/backupRotation.timer' 'https://raw.githubusercontent.com/thaibault/backupRotation/master/backupRotation.service')
+source=('https://raw.githubusercontent.com/thaibault/backupRotation/master/backupRotation.sh' \
+        'https://raw.githubusercontent.com/thaibault/backupRotation/master/backupRotation.timer' \
+        'https://raw.githubusercontent.com/thaibault/backupRotation/master/backupRotation.service')
 md5sums=('SKIP' 'SKIP' 'SKIP')
 
 package() {
-    #for url in "${source[@]}"; do
-    #    wget "$url"
-    #done
-    install -D --mode 755 backupRotation.sh "${pkgdir}/usr/bin/backupRotation"
-    install -D --mode 755 backupRotation.service "${pkgdir}/etc/systemd/system/"
-    install -D --mode 755 backupRotation.timer "${pkgdir}/etc/systemd/system/"
+    install -D --mode 755 "${srcdir}/backupRotation.sh" \
+        "${pkgdir}/usr/bin/backupRotation"
+    install -D --mode 755 "${srcdir}/backupRotation.service" \
+        "${pkgdir}/etc/systemd/system/backupRotation.service"
+    install -D --mode 755 "${srcdir}/backupRotation.timer" \
+        "${pkgdir}/etc/systemd/system/backupRotation.timer"
 }
 # region vim modline
 # vim: set tabstop=4 shiftwidth=4 expandtab:
--- backupRotation.sh
+++ backupRotation.sh
@@ -32,39 +32,37 @@
 # Short-Description: see above
 # Description:       see above
 ### END INIT INFO
-__NAME__='backupRotation'
 # endregion
-backupRotation() {
-    # Provides the main module scope.
-    # region constants
-    local sourcePath='/tmp/source/'
-    local targetPath='/tmp/backup/'
-    local eMailAddress='' # Notification are disabled by default.
-    local dailyTargetPath='daily/'
-    local weeklyTargetPath='weekly/'
-    local monthlyTargetPath='monthly/'
-    local targetDailyFileName="$(date +'%d-%m-%Y')"
-    local targetWeeklyFileName="$(date +'%V sav. %m-%Y')"
-    local targetMonthlyFileName="$(date +'%m-%Y')"
-    local backupWeekDayNumber=6 # Saturday
-    local backupMonthDayNumber=1
-    local numberOfDailyRetentionDays=14 # Daily backups for the last 14 days.
-    local numberOfWeeklyRetentionDays=56 # Weekly backups for the last 2 month.
-    local numberOfMonthlyRetentionDays=365 # Monthly backups for the last year.
-    local backupCommand='rsync --recursive --delete --perms --executability --owner --group --times --devices --specials --acls --links --super --whole-file --force --protect-args --hard-links --max-delete=1 --progress --human-readable --itemize-changes --verbose "$sourcePath" "$targetFilePath" && tar --dereference --create --verbose --gzip --file "${targetFilePath}.tar.gz" "$targetFilePath" && rm --recursive --verbose "$targetFilePath"'
-    # Folder to delete is the last command line argument.
-    local cleanupCommand='rm --recursive --verbose'
-    # endregion
-    # region controller
-    # Get current month and week day number
-    local monthDayNumber="$(date +'%d')"
-    local weekDayNumber="$(date +'%u')"
-    # Check if source files exist and send an email if not
-    if [ ! -d "$sourcePath" ]; then
-        local date="$(date)"
-        local message="Source files on \"$sourcePath\" should be backed up but aren't available."
-        if [[ "$eMailAddress" != '' ]]; then
-            msmtp -t <<EOF
+# Provides the main module scope.
+# region constants
+sourcePath='/tmp/source/'
+targetPath='/tmp/backup/'
+eMailAddress='' # Notification are disabled by default.
+dailyTargetPath='daily/'
+weeklyTargetPath='weekly/'
+monthlyTargetPath='monthly/'
+targetDailyFileName="$(date +'%d-%m-%Y')"
+targetWeeklyFileName="$(date +'%V sav. %m-%Y')"
+targetMonthlyFileName="$(date +'%m-%Y')"
+backupWeekDayNumber=6 # Saturday
+backupMonthDayNumber=1
+numberOfDailyRetentionDays=14 # Daily backups for the last 14 days.
+numberOfWeeklyRetentionDays=56 # Weekly backups for the last 2 month.
+numberOfMonthlyRetentionDays=365 # Monthly backups for the last year.
+backupCommand='rsync --recursive --delete --perms --executability --owner --group --times --devices --specials --acls --links --super --whole-file --force --protect-args --hard-links --max-delete=1 --progress --human-readable --itemize-changes --verbose "$sourcePath" "$targetFilePath" && tar --dereference --create --verbose --gzip --file "${targetFilePath}.tar.gz" "$targetFilePath" && rm --recursive --verbose "$targetFilePath"'
+# Folder to delete is the last command line argument.
+cleanupCommand='rm --recursive --verbose'
+# endregion
+# region controller
+# Get current month and week day number
+monthDayNumber="$(date +'%d')"
+weekDayNumber="$(date +'%u')"
+# Check if source files exist and send an email if not
+if [ ! -d "$sourcePath" ]; then
+    date="$(date)"
+    message="Source files on \"$sourcePath\" should be backed up but aren't available."
+    if [[ "$eMailAddress" != '' ]]; then
+        msmtp -t <<EOF
 From: $eMailAddress
 To: $eMailAddress
 Reply-To: $eMailAddress
@@ -74,35 +72,29 @@ Subject: Source files doesn't exist.
 $message
 
 EOF
-        fi
-        echo "$message" &>/dev/stderr
-        return 1
     fi
-    if [[ "$monthDayNumber" == "$backupMonthDayNumber" ]]; then
-        local targetFilePath="${targetPath}${dailyTargetPath}${targetDailyFileName}"
-    elif [[ "$weekDayNumber" == "$backupMontheDayNumber" ]]; then
-        local targetFilePath="${targetPath}${dailyTargetPath}${targetDailyFileName}"
-    else
-        local targetFilePath="${targetPath}${dailyTargetPath}${targetDailyFileName}"
-    fi
-    mkdir --parents "$(dirname "$targetFilePath")"
-    eval "$backupCommand"
-    # Clean outdated daily backups.
-    find "$targetPath" -mtime +"$numberOfDailyRetentionDays" -type d -exec \
-        "$cleanupCommand" {} \;
-    # Clean outdated weekly backups.
-    find "$targetPath" -mtime +"$numberOfWeeklyRetentionDays" -type d -exec \
-        "$cleanupCommand" {} \;
-    # Clean outdated monthly backups.
-    find "$targetPath" -mtime +"$numberOfMonthlyRetentionDays" -type d -exec \
-        "$cleanupCommand" {} \;
-    # endregion
-    return $?
-}
-if [[ "$0" == *"${__NAME__}.sh" ]]; then
-    "$__NAME__" "$@"
-    exit $?
+    echo "$message" &>/dev/stderr
+    return 1
 fi
+if [[ "$monthDayNumber" == "$backupMonthDayNumber" ]]; then
+    targetFilePath="${targetPath}${dailyTargetPath}${targetDailyFileName}"
+elif [[ "$weekDayNumber" == "$backupMontheDayNumber" ]]; then
+    targetFilePath="${targetPath}${dailyTargetPath}${targetDailyFileName}"
+else
+    targetFilePath="${targetPath}${dailyTargetPath}${targetDailyFileName}"
+fi
+mkdir --parents "$(dirname "$targetFilePath")"
+eval "$backupCommand"
+# Clean outdated daily backups.
+find "$targetPath" -mtime +"$numberOfDailyRetentionDays" -type d -exec \
+    "$cleanupCommand" {} \;
+# Clean outdated weekly backups.
+find "$targetPath" -mtime +"$numberOfWeeklyRetentionDays" -type d -exec \
+    "$cleanupCommand" {} \;
+# Clean outdated monthly backups.
+find "$targetPath" -mtime +"$numberOfMonthlyRetentionDays" -type d -exec \
+    "$cleanupCommand" {} \;
+# endregion
 # region vim modline
 # vim: set tabstop=4 shiftwidth=4 expandtab:
 # vim: foldmethod=marker foldmarker=region,endregion:
