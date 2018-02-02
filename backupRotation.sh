ca48b1f3198112dd315f54d6f0b16b3b283ade68
torben <t.sickert[~at~]gmail.com>
2018-02-02 21:00:59 +0100

    Automatic update to new version.

--- backupRotation.sh
+++ backupRotation.sh
@@ -31,17 +31,16 @@ backupRotation__dependencies__=(
     test
 )
 # shellcheck disable=SC2034,SC2016
-bl_doctest__documentation__='
+backupRotation__documentation__='
     This module provides generic service handling for each program supporting
     standard process signals. In general you only have to replace the string
     "generic" to you specific application name.
 
-    You have to install program "msmtp" to get this script working. A proper
+    You have to install program `msmtp` to get this script working. A proper
     user specific "~/.msmtprc" or global "/etc/msmtprc" have to be present on
-    wating distribution. A sample configuration using simple gmail account to
-    send mails (Replace "ACCOUNT_NAME", "ACCOUNT_E_MAIL_ADDRESS",
-    "ACCOUNT_PASSWORD", "RECIPIENT_E_MAIL_ADDRESS", "URL1" and remove or edit
-    "URL2", "NODE_NAME" and "ANOTHER_RECIPIENT_E_MAIL_ADDRESS"):
+    your distribution. A sample configuration using simple gmail account to
+    send mails (Replace `ACCOUNT_NAME`, `ACCOUNT_E_MAIL_ADDRESS`,
+    `ACCOUNT_PASSWORD`, `RECIPIENT_E_MAIL_ADDRESS`):
 
     ```
         defaults
@@ -60,15 +59,23 @@ bl_doctest__documentation__='
 
         account        default : gmail
     ```
+
+    Furthermore you should create a file "/etc/backupRotation" to overwrite the
+    following variables. You need to set values for
+    `backupRotation_source_target_mappings` at least:
+
+    ```bash
+        declare -A backupRotation_source_target_mappings=(
+            ["SOURCE_URL1"]="TARGET_URL1 RECIPIENT_E_MAIL_ADDRESS"
+            ["SOURCE_URL2"]="TARGET_URL2 RECIPIENT_E_MAIL_ADDRESS ANOTHER_RECIPIENT_E_MAIL_ADDRESS"
+            ...
+        )
+    ```
 '
 ## region default options
-# Example:
-# declare -A backupRotation_source_target_mappings=(
-#     ['SOURCE_URL1']='TARGET_URL1 RECIPIENT_E_MAIL_ADDRESS'
-#     ['SOURCE_URL2']='TARGET_URL2 RECIPIENT_E_MAIL_ADDRESS ANOTHER_RECIPIENT_E_MAIL_ADDRESS'
-# )
 declare -A backupRotation_source_target_mappings=()
-backupRotation_sender_e_mail_address='' # Disables by default
+# Disables by e-mail sending if empty.
+backupRotation_sender_e_mail_address=''
 backupRotation_replier_e_mail_address="$sender_e_mail_address"
 backupRotation_daily_target_path=daily/
 backupRotation_weekly_target_path=weekly/
@@ -80,9 +87,12 @@ backupRotation_target_monthly_file_name="$target_daily_file_name"
 backupRotation_month_day_number=1
 # Should be in range 1 till 7
 backupRotation_week_day_number=6 # Saturday
-backupRotation_number_of_daily_retention_days=14 # Daily backups for the last 14 days.
-backupRotation_number_of_weekly_retention_days=56 # Weekly backups for the last 2 month.
-backupRotation_number_of_monthly_retention_days=365 # Monthly backups for the last year.
+# Creates daily backups for the last 14 days.
+backupRotation_number_of_daily_retention_days=14
+# Creates weekly backups for the last 2 month.
+backupRotation_number_of_weekly_retention_days=56
+# Creates monthly backups for the last year.
+backupRotation_number_of_monthly_retention_days=365
 backupRotation_target_file_extension=.tar.gz
 backupRotation_command='rsync --recursive --delete --perms --executability --owner --group --times --devices --specials --acls --links --super --whole-file --force --protect-args --hard-links --max-delete=1 --progress --human-readable --itemize-changes --verbose --exclude=.git --exclude=.npm --exclude=node_modules --exclude=.local "$source_path" "$target_file_path" && tar --create --verbose --gzip --file "${target_file_path}${target_file_extension}" "$target_file_path"; rm --recursive --verbose "$target_file_path"'
 backupRotation_post_run_command=''
@@ -100,7 +110,8 @@ fi
 # endregion
 # region functions
 ## region controller
-main() {
+alias backupRotation.main=backupRotation_main
+backupRotation_main() {
     # Get current month and week day number
     local month_day_number="$(
         date +'%d' | grep '[1-9][0-9]?' --only-matching --extended-regexp)"
